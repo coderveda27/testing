aada089bd963834d3d2e2d4e9439015900e23fba
Veda Kesarwani <vedakesarwani@gmail.com>
2025-10-24 00:25:48 -0400

    added testing file with comments about procedure

---
+++ testing.py
@@ -0,0 +1,6 @@
+# I'm just experimenting with version control
+# I've cloned a repo from GitHub into my local
+# I've used the VS terminal to create my own branch 
+# git checkout -b veda-feature
+# now i can experiment and add my own features without changing directly on main
+# after adding changes, I can commit them and have a push them onto the main branch (after PR approval)
